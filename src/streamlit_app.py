756de18f52d05d400cc64d9b1c72bb75f5fc6b25
Evan Arlian <evanarlian2000@gmail.com>
2022-09-08 16:08:40 +0700

    fix io

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,13 +1,17 @@
+from tempfile import NamedTemporaryFile
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
+import soundfile
 import librosa
+import librosa.display
 import matplotlib.pyplot as plt
 import streamlit as st
 
 
 
-def input_form() -> tuple:
+def input_form() -> Tuple[np.ndarray, int, str]:
     """
     Performs audio loading related tasks.
     Returns tuple of (audio_data, sample_rate, filename)
@@ -31,7 +35,9 @@ def input_form() -> tuple:
     # load audio (will be converted to mono by default)
     try:
         audio_arr, sr = librosa.load(
-            uploaded_file if uploaded_file is not None else audio_samples[selected_samples],
+            uploaded_file
+            if uploaded_file is not None
+            else audio_samples[selected_samples],
             sr=22050 if resample else None,
         )
     except Exception as e:
@@ -43,28 +49,43 @@ def input_form() -> tuple:
 
 
 def visualize_wave(audio_arr: np.ndarray, sr: int, audio_name: str) -> None:
-    
-    # just the waveform
-    fig_wave, ax_wave = plt.subplots()
-    librosa.display.waveshow(audio_arr, sr=sr, ax=ax_wave)
-    st.pyplot(fig_wave)
 
-    # log melspec
-    fig_mel, ax_mel = plt.subplots()
+    # plot wave on top and mel spec on the bottom
+    fig, axs = plt.subplots(2, constrained_layout=True)
+    librosa.display.waveshow(audio_arr, sr=sr, ax=axs[0])
+    axs[0].set_ylabel("Amplitude")
     mel_spec = librosa.feature.melspectrogram(y=audio_arr, sr=sr)
     db_mel_spec = librosa.amplitude_to_db(mel_spec)
-    librosa.display.specshow(db_mel_spec, ax=ax_mel)
-    st.pyplot(fig_mel)
-    # TODO add more info like: axis label, better color, librosa built in stuff, etc
+    librosa.display.specshow(db_mel_spec, ax=axs[1], x_axis="time", y_axis="mel")
+    fig.suptitle(audio_name)
+    st.pyplot(fig)
+
+    # saving to a fake file for streamlit audio
+    with NamedTemporaryFile(suffix=".wav") as temp:
+        soundfile.write(temp, audio_arr, sr)
+        with open(temp.name, "rb") as f:
+            audio_bytes = f.read()
+    st.audio(audio_bytes)
 
 
 
 def main():
     st.title("🎹 Audiomentations Demo")
-    audio_arr, sr, audio_name = input_form()
-    st.write(audio_arr.shape, sr, audio_name)
 
+    # TODO choose aug here
+
+    audio_arr, sr, audio_name = input_form()
+    
+    st.header("Before")
     visualize_wave(audio_arr, sr, audio_name)
+
+    # TODO do aug here
+
+    st.header("After")
+    visualize_wave(audio_arr, sr, audio_name)
+
+
+    # TODO docs so we can understand augs + copy paste
 
 
 if __name__ == "__main__":
