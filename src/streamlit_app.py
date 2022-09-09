c708ae1c5a22021fcf4f9d07e168fb06eb5c17b1
Evan Arlian <evanarlian2000@gmail.com>
2022-09-09 16:04:50 +0700

    fix io for custom input

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,6 +1,6 @@
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, SpooledTemporaryFile
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, final
 
 import numpy as np
 import soundfile
@@ -9,6 +9,9 @@ import librosa.display
 import matplotlib.pyplot as plt
 import streamlit as st
 
+
+def aug_sidebar():
+    pass
 
 
 def input_form() -> Tuple[np.ndarray, int, str]:
@@ -21,7 +24,6 @@ def input_form() -> Tuple[np.ndarray, int, str]:
 
     # use form
     with st.form("input_form"):
-        st.header("Select audio")
         selected_samples = st.selectbox("Use sample audio...", audio_samples.keys())
         uploaded_file = st.file_uploader("...or upload you own.")
         st.caption("*All audio inputs will be converted to mono.*")
@@ -32,19 +34,29 @@ def input_form() -> Tuple[np.ndarray, int, str]:
     if not submitted:
         st.stop()
 
-    # load audio (will be converted to mono by default)
-    try:
-        audio_arr, sr = librosa.load(
-            uploaded_file
-            if uploaded_file is not None
-            else audio_samples[selected_samples],
-            sr=22050 if resample else None,
-        )
-    except Exception as e:
-        st.error(e)
-        st.stop()
+    # handle if user is using custom or sample song
+    if uploaded_file is not None:
+        # librosa cannot read .mp3 BytesIO but can read .mp3 files
+        # we need to fake create audio files with the matching suffix
+        audio_name = uploaded_file.name
+        temp = NamedTemporaryFile(suffix=Path(audio_name).suffix)
+        temp.write(uploaded_file.getvalue())
+        temp.seek(0)
+        audio_path = temp.name
+    else:
+        audio_name = selected_samples
+        audio_path = audio_samples[selected_samples]
 
-    audio_name = uploaded_file.name if uploaded_file is not None else selected_samples
+    # read audio file (either fake or real)
+    try:
+        audio_arr, sr = librosa.load(audio_path, sr=22050 if resample else None)
+    except Exception:
+        st.error(f"Error opening '{audio_name}'.")
+        st.stop()
+    finally:
+        if uploaded_file is not None:
+            temp.close()  # close (and auto delete) tempfile even if error occurs
+
     return audio_arr, sr, audio_name
 
 
@@ -60,30 +72,43 @@ def visualize_wave(audio_arr: np.ndarray, sr: int, audio_name: str) -> None:
     fig.suptitle(audio_name)
     st.pyplot(fig)
 
-    # saving to a fake file for streamlit audio
+    # we need to get the bytes for streamlit audio
+    # and soundfile cannot save to a BytesIO so we make a fake file
     with NamedTemporaryFile(suffix=".wav") as temp:
         soundfile.write(temp, audio_arr, sr)
-        with open(temp.name, "rb") as f:
-            audio_bytes = f.read()
+        temp.seek(0)
+        audio_bytes = temp.read()
     st.audio(audio_bytes)
 
 
-
 def main():
+
+    st.set_page_config(layout="wide")
     st.title("🎹 Audiomentations Demo")
 
     # TODO choose aug here
+    input_col, metadata_col = st.columns(2, gap="large")
 
-    audio_arr, sr, audio_name = input_form()
-    
-    st.header("Before")
-    visualize_wave(audio_arr, sr, audio_name)
+    with input_col:
+        st.header("Select audio")
+        audio_arr, sr, audio_name = input_form()
+
+    with metadata_col:
+        # TODO metadata
+        st.header("Input metadata")
+        st.write(audio_arr.shape[0] / sr)
 
     # TODO do aug here
 
-    st.header("After")
-    visualize_wave(audio_arr, sr, audio_name)
+    before_col, after_col = st.columns(2, gap="large")
 
+    with before_col:
+        st.header("Before")
+        visualize_wave(audio_arr, sr, audio_name)
+
+    with after_col:
+        st.header("After")
+        visualize_wave(audio_arr, sr, audio_name)
 
     # TODO docs so we can understand augs + copy paste
 
